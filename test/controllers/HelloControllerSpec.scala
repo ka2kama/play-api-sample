b5506e158385627d95f471e7b5711b71b7abd527
ka2kamaboko <49121261+ka2kamaboko@users.noreply.github.com>
2019-09-09 20:10:11 +0900

    「/hello」にGETとPOSTでアクセスできる

--- test/controllers/HelloControllerSpec.scala
+++ test/controllers/HelloControllerSpec.scala
@@ -2,18 +2,33 @@ package controllers
 
 import org.scalatestplus.play._
 import org.scalatestplus.play.guice._
-import play.api.test._
+import play.api.libs.json.Json
 import play.api.test.Helpers._
+import play.api.test._
 
-class HelloControllerSpec extends PlaySpec with GuiceOneAppPerTest with Injecting {
+class HelloControllerSpec extends PlaySpec with GuiceOneAppPerTest {
 
   "HelloController GET" should {
 
-    "「/hello」にGETメソッドでアクセスできる" in {
+    "「/hello」にGETメソッドでアクセスすると「Hello World」が返る"  in {
       val request = FakeRequest(GET, "/hello")
       val home = route(app, request).get
 
       status(home) mustBe OK
+      contentType(home) mustBe Some("text/plain")
+      contentAsString(home) mustBe "Hello World"
+    }
+  }
+
+  "HelloController POST" should {
+
+    "「/hello」にPOSTメソッドでアクセスするとJsonが返る"  in {
+      val request = FakeRequest(POST, "/hello")
+      val home = route(app, request).get
+
+      status(home) mustBe OK
+      contentType(home) mustBe Some("application/json")
+      contentAsJson(home) mustBe Json.obj("hello"-> "world", "language" -> "scala")
     }
   }
 }
